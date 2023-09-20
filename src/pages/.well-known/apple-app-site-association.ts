5669707b457908a1b1a4c6a866911ef2362c85af
Peter Ferguson <peterferguson95@gmail.com>
2023-09-20 10:44:56 +0100

    chore: update aasa

--- src/pages/.well-known/apple-app-site-association.ts
+++ src/pages/.well-known/apple-app-site-association.ts
@@ -1,10 +1,9 @@
 // - comma separated list of app IDs
 const APP_SUBDOMAINS = import.meta.env.APP_SUBDOMAINS ?? "";
-const URL = import.meta.env.VERCEL_URL ?? "";
-// const HOST = import.meta.env.PUBLIC_HOST
-if (!URL) throw new Error("Missing URL env var");
+const HOSTNAME = import.meta.env.PUBLIC_HOSTNAME;
+if (!HOSTNAME) throw new Error("Missing HOSTNAME env var");
 
-const BUNDLE_ID = URL.replace("https://", "").replace("/", "").split(".").reverse().join(".");
+const BUNDLE_ID = HOSTNAME.replace("https://", "").replace("/", "").split(".").reverse().join(".");
 const PERSONAL_ID = "3ZDVH7APQS";
 const APP_ID = `${PERSONAL_ID}.${BUNDLE_ID}`;
 const appIds = [APP_ID, ...APP_SUBDOMAINS.split(",").flatMap((subdomain) => [`${APP_ID}.${subdomain}`])];
