df2601fb0347500fec216dd29adf523defa34ece
Peter Ferguson <peterferguson95@gmail.com>
2023-09-20 14:24:27 +0100

    chore: change to server out

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite'
+const { PUBLIC_HOSTNAME } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'never',
+  site: `https://${PUBLIC_HOSTNAME}`,
+	output: 'server',
+	trailingSlash: 'never',
   integrations: [react(), tailwind()]
 });
